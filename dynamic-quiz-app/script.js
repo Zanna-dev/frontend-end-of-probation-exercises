9ac251d8b1e4baab43f4327e0f52aec17ba261a4
Zanna-dev <pamn2704@gmail.com>
2025-06-19 12:45:46 +0100

    Create JS File

---
+++ dynamic-quiz-app/script.js
@@ -0,0 +1,17 @@
+const quizData = [{
+    question: "What is the Capital of France?",
+    Options:["Lodon", "Berlin", "Paris", "Rome"],
+    correct: "Paris"
+},
+ {
+   question: "Which language runs in a web browser",
+    Options:["Python", "C", "Java", "JavaScript"],
+    correct: "JavaScript"
+},
+ {
+    question: "What does Css stands for?",
+    Options:["Central Serial sever", "Coding seek sever", "Computer styled syntax", "Cascading style sheets"],
+    correct: "Cascading style sheets"
+ }
+];
+
